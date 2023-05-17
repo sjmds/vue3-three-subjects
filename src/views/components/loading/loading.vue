5f38f912cc7969bd54e85d68e3794315b5ea9347
sjm <sunjianming@example.com>
2023-05-17 09:57:32 +0800

    loading

--- src/views/components/loading/loading.vue
+++ src/views/components/loading/loading.vue
@@ -1,7 +1,15 @@
 <template>
-  <FullscreenLoading />
+  <!-- <FullscreenLoading /> -->
+  <div ref="ss">content</div>
 </template>
 
 <script setup lang="ts">
+import { ref, render,h,onMounted } from 'vue'
 import FullscreenLoading from '@/components/FullscreenLoading'
+const ss = ref<any>(null)
+onMounted(()=>{
+  // "render"函数的返回值需要是"VNode"格式, "h"函数可以构造"VNode"格式数据. 
+  // ss.value是需要append的dom，render不会替换ss.value的内容
+  render(h(FullscreenLoading), ss.value)
+})
 </script>
