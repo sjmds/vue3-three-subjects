86f4b8cec2fbb1d29621438e3cba03d8a340fe2e
sjm <sunjianming@example.com>
2023-05-23 16:46:37 +0800

    3d

--- src/views/subjects/three/models3D.vue
+++ src/views/subjects/three/models3D.vue
@@ -11,6 +11,7 @@ import path from 'pathe'
 import { ref, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { useCommonStore } from '@/stores/common'
@@ -21,32 +22,34 @@ let camera:any, scene:any, renderer:any
 
 const init = () => {
 	scene = new THREE.Scene()
-	camera = new THREE.PerspectiveCamera( 45, innerWidth.value / innerHeight.value, 1, 500 )
-  camera.position.set( 0, 0, 100 )
+	camera = new THREE.PerspectiveCamera( 45, innerWidth.value / innerHeight.value, 0.1, 100 )
+  camera.position.set( 1.5, 4, 9 )
   camera.lookAt( 0, 0, 0 )
 
   //
 
   const dracoLoader = new DRACOLoader()
-  dracoLoader.setDecoderPath( path.resolve('node_modules/three/examples/jsm/libs/draco/gltf/') )
+  dracoLoader.setDecoderPath( path.join('/node_modules/three/examples/jsm/libs/draco/gltf/') )
 
   const loader = new GLTFLoader()
   loader.setDRACOLoader( dracoLoader )
-  loader.setPath( path.resolve('node_modules/three/examples/models/gltf/AVIFTest/') )
-  loader.load('/forest_house.glb', function ( gltf ) {
-
+  loader.setPath( path.resolve('static/gltf/') )
+  loader.load('/forest_house.glb', ( gltf ) => {
     scene.add( gltf.scene )
-
     render()
+	} )
 
-  } )
-
-  //
+  // 
 
 	renderer = new THREE.WebGLRenderer()
 	renderer.setSize( innerWidth.value, innerHeight.value )
 	sceneDom.value.appendChild(renderer.domElement)
-	
+
+  const controls = new OrbitControls( camera, renderer.domElement )
+  controls.addEventListener( 'change', render )
+  controls.target.set( 0, 2, 0 )
+  controls.update()
+
   window.addEventListener( 'resize', onWindowResize )
 }
 function animate() {
